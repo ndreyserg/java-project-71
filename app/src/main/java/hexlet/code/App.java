71f97d940bc2cc473e16da734fd149fae0ba2ff2
ndreyserg <ndreyserg@gmail.com>
2025-12-03 21:07:15 +0500

    Установлен picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,24 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import java.util.concurrent.Callable;
 
-public class App {
+
+@Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference."
+)
+public class App implements Callable<Integer>{
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
     }
 }
 
