1505bf7aaef907d05018f0ce94319e380220dfbb
ndreyserg <ndreyserg@gmail.com>
2026-01-08 09:29:27 +0500

    Добавлена справка

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,10 @@
 package hexlet.code;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
 import java.util.concurrent.Callable;
 
 
@@ -9,7 +13,17 @@ import java.util.concurrent.Callable;
         mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference."
 )
-public class App implements Callable<Integer>{
+public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format",  defaultValue = "stylish",
+            description = "output format [${DEFAULT-VALUE}]")
+    private String format;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
